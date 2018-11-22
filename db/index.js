3c2f0b734d094fadc1d72bc2556b361e7fa957d9
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-22 10:52:02 -0300

    Implementación de un módulo básico de base de datos

--- db/index.js
+++ db/index.js
@@ -1,6 +1,19 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel) /* hasMany -> AgentModel tiene muchas MetricModel */
+  MetricModel.belongsTo(AgentModel) /* belongsTo -> MetricModel pertener a un AgentModel */
+
+  await sequelize.authenticate() /* Se conecta a la BD. authenticate es una promise */
+
   const Agent = {}
   const Metric = {}
 
