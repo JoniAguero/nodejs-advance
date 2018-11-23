4ffddd1358bd9819d435084d264ef858fc80ff55
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-23 07:26:04 -0300

    Update agent-tests.js
    Notas de sinon, proxyquire, etc..

--- db/tests/agent-tests.js
+++ db/tests/agent-tests.js
@@ -7,22 +7,22 @@ const proxyquire = require('proxyquire')
 let config = {
     logging() {}
 }
-
+/* Imito el modelo de Agent en index */
 let MetricStub = {
     belongsTo: sinon.spy()
 }
 
 let AgentStub = null
 let db = null
-let sandbox = null
+let sandbox = sinon.createSandbox()
 
 test.beforeEach(async () => {
-    sandbox = sinon.sandbox.create()
-
+    sandbox = sinon.createSandbox()
+    /* Imito el modelo de Agent en index */
     AgentStub = {
         hasMany: sandbox.spy()
     }
-
+    /* proxyquire -> me permite hacer require un módulo y sobreescribirlos */
     const setupDatabase = proxyquire('../', {
         './models/agent': () => AgentStub,
         './models/metric': () => MetricStub
@@ -32,16 +32,16 @@ test.beforeEach(async () => {
 })
 
 test.afterEach(() => {
-    sandbox && sinon.sandbox.restore()
+    sandbox && sandbox.restore()
 })
 
 test('Agent', t => {
     t.truthy(db.Agent, 'Agent service should exist')
 })
-
+/* test.serial -> Garantiza que nuestro entorno de Stub no esté saturado. Indicamos que los test se haran en serie. */
 test.serial('Setup', t => {
-    t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
-    t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+    t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed') /* Test que hasMany fue ejecutada */
+    t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')  /* Test que hasMany fue ejecutada con el argumento*/
     t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
     t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
 })
