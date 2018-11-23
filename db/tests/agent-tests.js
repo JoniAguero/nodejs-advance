49da3c660e2a326ac51bcbfadad413eec812b7f7
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-23 09:16:10 -0300

    Update agent-tests.js

--- db/tests/agent-tests.js
+++ db/tests/agent-tests.js
@@ -55,7 +55,11 @@ test.serial('Setup', t => {
 })
 
 test.serial('Agent#findById', async t => {
-  let agent = await db.Agent.findById(id)
+    let agent = await db.Agent.findById(id)
 
-  t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
+    t.true(AgentStub.findById.called, 'findById should be called on model')
+    t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+    t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
+
+    t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
