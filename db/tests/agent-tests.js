ddda145ad5d8bb370c249f2f68db4547ed561e78
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-23 09:10:37 -0300

    Implementación de findbyId y pruebas en el servicio Agent
    Ejemplo:
    
    var sinon = require('sinon)
    var findById = sinon.stub()
    findById.withArgs(1).returns('joni) -> Le decimos que cuándo llamemos a la funcion con el parametro 1, debe returnar 'joni'. Por lo tanto, cada vez que llamemos a esa función, retornará 'joni'

--- db/tests/agent-tests.js
+++ db/tests/agent-tests.js
@@ -27,6 +27,10 @@ test.beforeEach(async () => {
     hasMany: sandbox.spy()
   }
 
+  // Model findById Stub
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id))) /* Cuando ejecutamops finbyId con el parametro id (withArgs), esta tiene que retornar (returns) una promesa que resuelve agentFixtures.byId(id) */
+
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
     './models/metric': () => MetricStub
