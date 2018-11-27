372c995f85dbdae5423ccf7fe93e8a40c32504bb
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-27 08:55:14 -0300

    Almacenar la información del agente y reportar el agente conectado
    Para implementar la persistencia de los mensajes en la base de datos necesitamos tener una referencia de todos los agentes que tenemos conectados. Tenemos que crear un mapa de JavaScript.
    Cada vez que un cliente se conecte vamos a capturar el ID y la información del agente. Por ahora no conocemos la información porque no hemos recibido el primer mensaje, así que más adelante le vamos a setear la información.
    
    Después vamos a empezar a trabajar con los mensajes que publican en nuestro servidor. Para eso vamos a optener el topic y hacer un switch.
    El switch es una estructura de control de JS que permite ejecutar un código.

--- mqtt/server.js
+++ mqtt/server.js
@@ -7,6 +7,8 @@ const chalk = require('chalk')
 const db = require('db')
 const config = require('../db/config.setup')(false)
 
+const { parsePayload } = require('./utils')
+
 const backend = {
   type: 'redis',
   redis,
@@ -19,6 +21,7 @@ const settings = {
 }
 
 const server = new mosca.Server(settings)
+const clients = new Map()
 
 let Agent, Metric
 
@@ -30,9 +33,50 @@ server.on('clientDisconnected', client => {
   debug(`Client Disconnected: ${client.id}`)
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Received: ${packet.topic}`)
-  debug(`Payload: ${packet.payload}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        // Notify Agent is Connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+      break
+  }
 })
 
 server.on('ready', async () => {
@@ -50,6 +94,11 @@ function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[error]')} ${err.message}`)
+  console.error(err.stack)
 }
 
 process.on('uncaughtException', handleFatalError)
