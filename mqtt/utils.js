fc83b9a34918f66caee55b7aa7298203c8219e10
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-27 09:06:03 -0300

    Update utils.js

--- mqtt/utils.js
+++ mqtt/utils.js
@@ -1,19 +1,19 @@
 'use strict'
 
-function parsePayload(payload) {
-    if (payload instanceof Buffer) {
-        payload = payload.toString('utf8')
-    }
+function parsePayload (payload) {
+  if (payload instanceof Buffer) {
+    payload = payload.toString('utf8')
+  }
 
-    try {
-        payload = JSON.parse(payload)
-    } catch (e) {
-        payload = null
-    }
+  try {
+    payload = JSON.parse(payload)
+  } catch (e) {
+    payload = null
+  }
 
-    return payload
+  return payload
 }
 
 module.exports = {
-        parsePayload
+  parsePayload
 }
