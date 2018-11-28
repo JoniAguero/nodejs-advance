322d62916b75fdee71a8145874280ff96d646a4e
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-28 07:00:02 -0300

    Definición de rutas

--- api/serve.js
+++ api/serve.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
 
+app.use('/api', api)
+
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
 })
