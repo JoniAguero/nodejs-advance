10231809f02a042797049e650b49e7d688811f49
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-30 07:48:13 -0300

    Update server.js

--- web/server.js
+++ web/server.js
@@ -4,9 +4,11 @@ const debug = require('debug')('verse:web')
 const http = require('http')
 const path = require('path')
 const express = require('express')
+const asyncify = require('express-asyncify')
 const socketio = require('socket.io')
 const chalk = require('chalk')
-const PlatziverseAgent = require('agent')
+const verseAgent = require('verse-agent')
+
 const proxy = require('./proxy')
 const {
   pipe
@@ -16,7 +18,7 @@ const port = process.env.PORT || 8080
 const app = asyncify(express())
 const server = http.createServer(app)
 const io = socketio(server)
-const agent = new PlatziverseAgent()
+const agent = new verseAgent()
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', proxy)
@@ -28,7 +30,22 @@ io.on('connect', socket => {
   pipe(agent, socket)
 })
 
-function handleFatalError (err) {
+// Express Error Handler
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({
+      error: err.message
+    })
+  }
+
+  res.status(500).send({
+    error: err.message
+  })
+})
+
+function handleFatalError(err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
@@ -38,6 +55,6 @@ process.on('uncaughtException', handleFatalError)
 process.on('unhandledRejection', handleFatalError)
 
 server.listen(port, () => {
-  console.log(`${chalk.green('[verse-web]')} server listening on port ${port}`)
+  console.log(`${chalk.green('[platziverse-web]')} server listening on port ${port}`)
   agent.connect()
 })
